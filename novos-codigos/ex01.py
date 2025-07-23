65d17ca058d672188b48186250d234cf9db79607
Adalberto Carlos de Oliveira Júnior <adalbertomc123@gmail.com>
2025-07-22 22:43:14 -0300

    main

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,26 @@
-# Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+
+# Neste algoritmo, crie uma variável que armazena varias string e sua quantidade.
+
+
+print("Criar uma Lista de Comprar")
+lista_strings = []
+quantidades = []
+contador = 0        
+
+while True:
+    item = input("Digite um item para a lista (ou 'sair' para finalizar): ")
+    if item.lower() == 'sair':
+        break
+    quantidade = int(input(f"Digite a quantidade de '{item}': "))
+    lista_strings.append(item)
+    quantidades.append(quantidade)
+
+    contador += 1
+
+    
+print("Itens na lista e suas quantidades:")
+for item, qtd in zip(lista_strings, quantidades):
+    print(f"{item}: {qtd}")
+
+print("Quantidade total de itens:", sum(quantidades))
